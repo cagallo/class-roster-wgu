a69db6c0e49ace2d53adf526741abe29e3b581cf
Chez Gallo <cesaregallo0@gmail.com>
2024-05-06 23:47:48 -0600

    Initial Commit

---
+++ class-roster-wgu/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  class-roster-wgu
+//
+//  Created by Cesare Gallo on 5/6/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
